73940c5c1614a9977a8dd8c37f0427c9e5c345e9
ChaeSongi <a01050301239@gmail.com>
2018-08-09 11:11:01 +0900

    커밋

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rtmbot import RtmBot
 from rtmbot.core import Plugin
-
+import random
 import secret
 
 
@@ -8,8 +8,8 @@ class HelloPlugin(Plugin):
     def process_message(self, data):
         if "송이" in data["text"]:
             self.outputs.append([data["channel"], "네~?"])
-        elif "주사위" in data["text"]:
-            die = random.randint(1, 6)
+        elif "주사위" == data["text"]:
+            die = str(random.randint(1, 6))
             self.outputs.append([data["channel"], die])
         else:
             pass
